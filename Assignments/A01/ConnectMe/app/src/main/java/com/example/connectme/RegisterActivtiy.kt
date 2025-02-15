15373f02f2fe8e66ca4377806cb71e276777b6b0
blueork <114741623+blueork@users.noreply.github.com>
2025-02-15 16:55:40 +0500

    Implement the Register Screen and add btn func.
    Link the Login screen with the Register Screen and vice versa.

--- Assignments/A01/ConnectMe/app/src/main/java/com/example/connectme/RegisterActivtiy.kt
+++ Assignments/A01/ConnectMe/app/src/main/java/com/example/connectme/RegisterActivtiy.kt
@@ -1,28 +1,32 @@
 package com.example.connectme
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.text.method.LinkMovementMethod
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivtiy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register_activtiy)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val registrationButton = findViewById<Button>(R.id.registration_button)
-        registrationButton.setOnClickListener{
-            val intent = Intent(this, RegisterActivtiy::class.java)
+        val textView = findViewById<TextView>(R.id.registration_notice)
+        textView.movementMethod = LinkMovementMethod.getInstance();
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
