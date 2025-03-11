5a10fcaba48a4298f6efed9c72d8ed3510190c8a
blueork <114741623+blueork@users.noreply.github.com>
2025-03-11 22:14:06 +0500

    Basic Shared Preference with a counter

--- Class_Tasks/app/src/main/java/com/example/firstapp/MainActivityRV.kt
+++ Class_Tasks/app/src/main/java/com/example/firstapp/MainActivityRV.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivityRV : AppCompatActivity() {
+    var c = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,38 +25,74 @@ class MainActivityRV : AppCompatActivity() {
 //            insets
 //        }
 
-        var rv=findViewById<RecyclerView>(R.id.rv)
-        var add=findViewById<RelativeLayout>(R.id.add)
-        var list= mutableListOf<Model>()
+        var add = findViewById<RelativeLayout>(R.id.add)
+        var count = findViewById<TextView>(R.id.count)
+
+        // this runs on ly when the onCreate function is called at the start
+        // afterwards on pause and on resume function handle the shared preference
+        var pref = getSharedPreferences("count", MODE_PRIVATE)
+        c = pref.getInt("c",0)
+
+        count.setText(c.toString())
+
+        add.setOnClickListener{
+            c++
+            count.setText(c.toString())
+        }
+
+//        var rv=findViewById<RecyclerView>(R.id.rv)
+//        var add=findViewById<RelativeLayout>(R.id.add)
+//        var list= mutableListOf<Model>()
 //        list.add(Model("Jhon Doe","0992","jhon.doe@gmail.com"))
 //        list.add(Model("Will Smith","2990","will.smith@gmail.com"))
 //        list.add(Model("Jada Smith","1122","jada.smith@gmail.com"))
 
-        var adapter=MyAdapter(list,this)
-        var lm= LinearLayoutManager(this)
-        rv.layoutManager=lm
-        rv.adapter=adapter
+//        var adapter=MyAdapter(list,this)
+//        var lm= LinearLayoutManager(this)
+//        rv.layoutManager=lm
+//        rv.adapter=adapter
+//
+//        var result =registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//            if (it.resultCode== RESULT_OK) {
+//                var dataa=it.data
+//
+//                var name = dataa?.getStringExtra("name")
+//                Log.d("name", name!!)
+//                var rollnum = dataa?.getStringExtra("rollnum")
+//                Log.d("rollnum",rollnum!!)
+//                var email = dataa?.getStringExtra("email")
+//                Log.d("email",email!!)
+//                var image = dataa?.getStringExtra("image")
+//                list.add(Model(name!!,rollnum!!,email!!,image!!))
+//                adapter.notifyDataSetChanged()
+//            }
+//        }
+//
+//        add.setOnClickListener {
+//            var intent= Intent(this,AddContact::class.java)
+//            result.launch(intent)
+//        }
 
-        var result =registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode== RESULT_OK) {
-                var dataa=it.data
+    }
 
-                var name = dataa?.getStringExtra("name")
-                Log.d("name", name!!)
-                var rollnum = dataa?.getStringExtra("rollnum")
-                Log.d("rollnum",rollnum!!)
-                var email = dataa?.getStringExtra("email")
-                Log.d("email",email!!)
-                var image = dataa?.getStringExtra("image")
-                list.add(Model(name!!,rollnum!!,email!!,image!!))
-                adapter.notifyDataSetChanged()
-            }
-        }
+    override fun onStart() {
+        super.onStart()
+        var pref = getSharedPreferences("count", MODE_PRIVATE)
+        c = pref.getInt("c",0)
+    }
 
-        add.setOnClickListener {
-            var intent= Intent(this,AddContact::class.java)
-            result.launch(intent)
-        }
+    override fun onResume() {
+        super.onResume()
+        var pref = getSharedPreferences("count", MODE_PRIVATE)
+        c = pref.getInt("c",0)
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        var pref = getSharedPreferences("count", MODE_PRIVATE)
+        var edit = pref.edit()
+        edit.putInt("c",c)
+        edit.apply()
     }
 }
